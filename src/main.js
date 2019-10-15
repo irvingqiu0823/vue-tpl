a0d1dfab4ae5a69449a4537301e74be3bf4e514b
irvingqiu0823 <irvingqiu0823@163.com>
2019-10-15 17:40:20 +0800

    add: delete vuerouter from main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 import App from './App.vue'
 import router from './router'
 
-Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 new Vue({
